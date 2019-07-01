b9aa7322dcb0fbf34a6d723f10f5fc2f4eea69b4
young-geun <dudrmshub@gmail.com>
2019-07-01 21:57:33 +0900

    package document

---
+++ R/swatanomaly-package.R
@@ -0,0 +1,18 @@
+#' swatanomaly: various algorithms for anomaly detection
+#'
+#' @description
+#' swatanomaly provides some algorithms used by NSR anomaly detection projects.
+#' @docType package
+#' @name swatanomaly-package
+#' @aliases swatanomaly
+#' @author Young-geun Kim <\url{https://github.com/ygeunkim}>
+#' @useDynLib swatanomaly
+#' @importFrom Rcpp sourceCpp
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
