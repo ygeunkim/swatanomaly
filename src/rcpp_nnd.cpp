5d8bc70783e3381a8459cc21f26ca8430fc63ed8
young-geun <dudrmshub@gmail.com>
2019-07-20 00:07:06 +0900

    correcting nnd algorithm

--- src/rcpp_nnd.cpp
+++ src/rcpp_nnd.cpp
@@ -22,47 +22,6 @@ double sum_sq(NumericVector x) {
   return sum;
 }
 
-//' Euclidean NND between validation and training blocks
-//'
-//' @description
-//' This function computes NND corresponding to euclidean distance between validation sets and training sets built by cross-validation.
-//' @param x NumericMatrix validation set.
-//' @param y NumericMatrix training set.
-//' @return NumericVector length identical to the validation set
-//' @details
-//' Consider input x and y. Compute
-//' \deqn{\sqrt{\sum (x_{ij} - y_{ij})^2}}
-//' for each observation.
-//' Calculate Euclidean distance between a point in validation series versus each point in training series.
-//' Find minimum value. It is NND.
-//' @useDynLib swatanomaly
-//' @importFrom Rcpp sourceCpp
-//' @export
-// [[Rcpp::export]]
-NumericVector euc_nnd(NumericMatrix x, NumericMatrix y) {
-  int nx = x.nrow();
-  int px = x.ncol();
-  int ny = y.nrow();
-  int py = y.ncol();
-
-  NumericVector euc(ny);
-  NumericVector nnd(nx);
-
-  if (px != py) {
-    stop("x and y should be have same dimension");
-  }
-
-  for (int i = 0; i < nx; i++) {
-    for (int j = 0; j < ny; j++) {
-      euc[j] = sum_sq(x(i, _) - y(j, _));
-    }
-    nnd[i] = min(euc);
-  }
-
-  nnd = sqrt(nnd);
-  return nnd;
-}
-
 //' Remove row index of a matrix in C++
 //'
 //' @description
@@ -115,92 +74,180 @@ IntegerVector seq_rcpp(int from, int to) {
   return x;
 }
 
-//' Euclidean pdf in Rcpp
+//' Squared l2 Distance between two windows
 //'
 //' @description
-//' This function computes a euclidean NND pdf of two multivariate series using Rcpp. See details what it is.
-//' @param x NumericMatrix, column should indicate variable.
-//' @param partition int, equally partitioning the series.
-//' @param display_progress If TRUE, display a progress bar. By default, FALSE.
-//' @return NumericVector, NND pdf
+//' This function gives distance matrix between two windows in multivariate time series
+//' @param x NumericMatrix. first window.
+//' @param y NumericMatrix. second window.
+//' @return double. squared l2 distance matrix form.
 //' @details
-//' Implement k-fold cross-validation. Here, k is partition.
-//' First partitioning the series equally.
-//' One block is validation set, and the others are trainig.
-//' Next for each validation series, it calculates sqrt(sum((x_i - x_j)^2)) versus training series.
-//' Find the minimum result for each validation block. This is NND of each block.
-//' Finally, you can get NND for every block and this is pdf for NND.
-//' For \code{\link{detect_anomaly}}, this pdf is able to threshold.
-//' Threshold is a tail of pdf, e.g. 0.99.
-//' @seealso
-//'    \code{\link{euc_nnd}}
-//'    \code{\link{nnd_thr}}
-//'    \code{\link{detect_anomaly}}
+//' Compute
+//' \deqn{d = x_{ij} - y_{kl}}
+//' element-wise. After that,
+//' \deqn{\frac{\sqrt{\sum{d}}}{wp}}
+//' where w is the wize of window, and p is the number of variables.
 //' @references
 //' Yun, J.-H., Hwang, Y., Lee, W., Ahn, H.-K., & Kim, S.-K. (2018). \emph{Statistical Similarity of Critical Infrastructure Network Traffic Based on Nearest Neighbor Distances} (Vol. 11050, pp. 1–23). Presented at the Research in Attacks, Intrusions, and Defenses, Cham: Springer International Publishing. \url{http://doi.org/10.1007/978-3-030-00470-5_27}
 //' @useDynLib swatanomaly
 //' @importFrom Rcpp sourceCpp
 //' @export
 // [[Rcpp::export]]
-NumericVector euc_pdf(NumericMatrix x, int partition, bool display_progress = false) {
-  int div = x.nrow() / partition;
+double compute_euc(NumericMatrix x, NumericMatrix y) {
+  int win = x.nrow();
+  int px = x.ncol();
+  double dist = 0;
 
-  NumericVector euc(x.nrow());
+  if (win != y.nrow())
+    stop("x and y should have same column number");
 
-  Progress p(partition, display_progress);
+  if (px != y.ncol())
+    stop("x and y should have same row number");
 
-  for (int i = 0; i < partition; i++) {
+  NumericVector l2(win * px);
 
-    if (Progress::check_abort())
-      return -1.0;
-
-    p.increment();
-
-    euc[Range(i * div, i * div + div - 1)] = euc_nnd(x(Range(i * div, i * div + div - 1), _), row_erase(x, seq_rcpp(i * div, i * div + div - 1)));
+  for (int i = 0; i < win; i++) {
+    for (int j = 0; j < px; j++) {
+      l2[i * win + j] = pow(x(i, j) - y(i, j), 2);
+    }
   }
 
-  return euc;
+  dist = sqrt(sum(l2)) / (win * px);
+
+  return dist;
 }
 
-//' Windowed NNS in Rcpp
+// [[Rcpp::export]]
+NumericMatrix sub_mat(NumericMatrix x, NumericVector row, NumericVector col) {
+  NumericMatrix y(row.size(), col.size());
+
+  for (int r = 0; r < row.size(); r++) {
+    for (int c = 0; c < col.size(); c++) {
+      y(r, c) = x(row[r], col[c]);
+    }
+  }
+
+  return y;
+}
+
+// [[Rcpp::export]]
+NumericVector partnnd(
+  NumericMatrix data,
+  int win,
+  int jump,
+  NumericVector partition,
+  int base_id,
+  Function d
+) {
+  NumericVector x_id = partition[partition == base_id];
+  NumericVector y_id = partition[partition != base_id];
+
+  int x_win = (x_id.size() - win) / jump + 1;
+  NumericVector x_nnd(x_win);
+
+  int y_win = (y_id.size() - win) / jump + 1;
+  NumericVector y_nnd(y_win);
+
+  NumericMatrix x(x_id.size(), data.ncol());
+  NumericMatrix y(y_id.size(), data.ncol());
+
+  for (int i = 0; i < x_win; i++) {
+    for (int j = 0; j < y_win; j++) {
+      y_nnd[j] = as<double>(d(x(Range(i * jump, i * jump + win - 1), _), y(Range(j * jump, j * jump + win - 1), _)));
+    }
+    x_nnd[i] = min(y_nnd);
+  }
+
+  return x_nnd;
+}
+
+//' Compute NND of normal data given window distance function
 //'
 //' @description
-//' This function computes a windowed NNS.
-//' Compute NND sliding window across given series.
-//' @param data NumericMatrix multivariate data set
-//' @param win int window size for sliding window
+//' This function computes NND of a normal data set corresponding to defined window distance.
+//' @param data NumericMatrix. data to be calculated NND.
+//' @param part int. the number of partition.
+//' @param win int. window size.
+//' @param jump int. shift size.
+//' @param d Function. distance function. By default, \code{\link{compute_euc}}. Arguments must be two matrices and the output double.
 //' @param display_progress If TRUE, display a progress bar. By default, FALSE.
-//' @return NumericVector, NND for each window index (index represented by its starting point)
+//' @return NumericVector. NND for each window. Its size is affected by "jump".
 //' @details
-//' Given n x p data, partition a window.
-//' Compute NND for each pair of window.
-//' The method is similar to \code{\link{euc_pdf}}.
-//' Note that the number of windows is nrow - win + 1 given size of window, win.
-//' @references
-//' Yun, J.-H., Hwang, Y., Lee, W., Ahn, H.-K., & Kim, S.-K. (2018). \emph{Statistical Similarity of Critical Infrastructure Network Traffic Based on Nearest Neighbor Distances} (Vol. 11050, pp. 1–23). Presented at the Research in Attacks, Intrusions, and Defenses, Cham: Springer International Publishing. \url{http://doi.org/10.1007/978-3-030-00470-5_27}
+//' First partition the whole given data set by "part".
+//' Without the loss of generality, consider the first window.
+//' We do not compute the distances between windows in the same window, assuming they are similar to each other.
+//' Then for the first window, we consider every other slided window in the other partion.
+//' Compute their distances. The minimum among the distance values is NND of the first window.
+//' Repeat this procedure for every window in the partition.
+//' Same for the other partition.
 //' @useDynLib swatanomaly
 //' @importFrom Rcpp sourceCpp
 //' @export
 // [[Rcpp::export]]
-NumericVector nns_cpp(NumericMatrix data, int win, bool display_progress = false) {
+NumericVector nnd_normal(
+  NumericMatrix data,
+  int part,
+  int win,
+  int jump,
+  Function d = compute_euc,
+  bool display_progress = false
+) {
   int n = data.nrow();
-  int win_num = n / win;
+  int part_num = n / part;
+  int part_rem = n % part;
 
-  NumericVector sliding(win_num);
-  NumericVector distvec(n);
+  NumericMatrix x(win, data.ncol()); // window
+  NumericMatrix y(win, data.ncol()); // versus window
 
-  Progress p(win_num, display_progress);
+  Progress p(part, display_progress);
 
-  for (int i = 0; i < win_num; i++) {
+  IntegerVector id = seq_len(part) - 1;
+  IntegerVector idx = rep_each(id, part_num); // partition index
+  IntegerVector idy(idx.size() + part_rem);
+  idy[Range(0, idx.size() - 1)] = idx;
+  if (part_rem != 0)
+    idy[Range(idx.size(), idx.size() + part_rem - 1)] = rep(part - 1, part_rem);
 
-    if (Progress::check_abort())
-      return -1.0;
+  NumericVector nnd(idy.size());
 
-    p.increment();
+  for (int i = 0; i < part; i++)
+    nnd[Range(i * jump, i * jump + win - 1)] = partnnd(data, win, jump, idy, i, d);
 
-    distvec[Range(i * win, i * win + win - 1)] = euc_nnd(data(Range(i * win, i * win + win - 1), _), row_erase(data, seq_rcpp(i * win, i * win + win - 1)));
-  }
+  return nnd;
+}
 
-  return distvec;
+//' Compute NND for Online Data-set
+//'
+//' @description
+//' This function computes NND of the data set which consists of anomaly.
+//' @param data NumericMatrix. original data set.
+//' @param newdata NumericMatrix. updated data set.
+//' @param win int. window size.
+//' @param jump int. shift size.
+//' @param d Function. distance function. By default, \code{\link{compute_euc}}. Arguments must be two matrices and the output double.
+//' @param display_progress If TRUE, display a progress bar. By default, FALSE.
+//' @return NumericVector. NND for each window.
+//' @details.
+//' Measure a distance between windows in new data set and normal data set.
+//' For this, slide windows in both set. Compute the distances and record them.
+//' @useDynLib swatanomaly
+//' @importFrom Rcpp sourceCpp
+//' @export
+// [[Rcpp::export]]
+NumericVector pred_nnd(
+  NumericMatrix data,
+  NumericMatrix newdata,
+  int win,
+  int jump,
+  Function d = compute_euc,
+  bool display_progress = false
+) {
+  int n = data.nrow();
+  NumericMatrix dat_new(n + win, data.ncol());
+  NumericVector nnd_new((newdata.nrow() - win) / jump + 1);
+
+  IntegerVector id(n + win);
+  id[Range(0, n - 1)] = rep(0, n);
+  id[Range(n, n + win - 1)] = rep(1, win);
 }
 
